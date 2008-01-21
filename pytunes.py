d96ca2dd18941332e2cba7e7477455c72247ec3e
Mark Matienzo <mark@matienzo.org>
2008-01-21 05:11:32 +0000

    initial import
    git-svn-id: http://svn.matienzo.org/public/pytunes@390 9bbe1849-123c-0410-9313-83ec163a5258

---
+++ pytunes.py
@@ -0,0 +1,46 @@
+import web, appscript
+
+interface = appscript.app("iTunes")
+
+urls = (
+  '/', 'main',
+  '/prev', 'prev',
+  '/next', 'next',
+  '/playpause', 'playpause',
+
+)
+
+def curr():
+  track = interface.current_track()
+  return [track.artist(), track.name(), track.album()]
+
+class main:
+  def GET(self, query_string):
+    ct = " - ".join(curr())
+    print render.base(track = ct)
+    print render.bottom
+
+class prev:
+  def GET(self):
+    interface.previous_track()
+    ct = " - ".join(curr())
+    print render.base(track = ct)
+    print "<b>Previous track requested</b>"
+    print render.bottom
+    
+class next:
+    interface.next_track()
+    ct = " - ".join(curr())
+    print render.base(track = ct)
+    print "<b>Next track requested</b>"
+    print render.bottom
+
+class playpause:
+    interface.playpause()
+    ct = " - ".join(curr())
+    print render.base(track = ct)
+    print "<b>Toggled play/pause</b>"
+    print render.bottom
+
+web.webapi.internalerror = web.debugerror
+if __name__ == '__main__': web.run(urls, globals())
